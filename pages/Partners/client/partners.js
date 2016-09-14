1bb709273c97f2e741dc94276c9aa4abf89cd585
eliasrosenfeld <erosenfeld@brandeis.edu>
2016-09-14 14:58:26 -0400

    changed donors

--- pages/Partners/client/partners.js
+++ pages/Partners/client/partners.js
@@ -1,5 +1,5 @@
 const ourSponsors = [
- {name:"Google",amt:"$50 million"},
+ {name:"Brandeis University",amt:"$50 million"},
  {name:"Apple",amt:"$20 million"},
  {name:"Facebook",amt:"$40 million"},
 ]
