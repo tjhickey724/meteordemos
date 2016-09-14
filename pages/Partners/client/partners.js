7a1787832ad00756319d8eb8576284407e4f57ef
eliasrosenfeld <erosenfeld@brandeis.edu>
2016-09-14 15:06:21 -0400

    change sponsors to partners

--- pages/Partners/client/partners.js
+++
@@ -1,9 +0,0 @@
-const ourSponsors = [
- {name:"Brandeis University",amt:"$50 million"},
- {name:"Apple",amt:"$20 million"},
- {name:"Facebook",amt:"$40 million"},
-]
-
-Template.sponsors.helpers({
-	sponsorData: ourSponsors
-})
