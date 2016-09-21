6595e849bd12e073c6d570cf84670f4507a1d167
eliasrosenfeld <erosenfeld@brandeis.edu>
2016-09-21 14:37:04 -0400

    Spaced out title

---
+++ pages/Partners/client/partners.js
@@ -0,0 +1,9 @@
+const ourSponsors = [
+ {name:"Brandeis University",amt:"$50 million"},
+ {name:"Apple",amt:"$20 million"},
+ {name:"Facebook",amt:"$40 million"},
+]
+
+Template.sponsors.helpers({
+	sponsorData: ourSponsors
+})
