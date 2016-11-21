a76106385dc56872bdad643c3ed6a070d9c8aec5
tim <tjhickey@brandeis.edu>
2016-11-21 12:43:24 -0500

    myReviews ...

--- pages/reviews/client/mygame.js
+++ pages/reviews/client/mygame.js
@@ -1,5 +1,6 @@
 function userEmail(){
   var user = Meteor.user();
+  console.dir(user);
   if (user.services && user.services.google){
     return user.services.google.email;
   } else if (user.emails) {
@@ -13,11 +14,13 @@ Template.mygame.helpers({
   myreviews: function(){
     var myemail = userEmail();
     var myProfile = Members.findOne({email:myemail});
+    var team = 0;
+    if (myProfile) {team = myProfile.team;}
     console.log(myemail);
     console.dir(myProfile);
-    var myReviews = Reviews.find({team:myProfile.team});
+    var myReviews = Reviews.find({team:team});
     console.dir(myReviews.fetch());
-    if (myemail == "tjhickey@brandeis.edu"){
+    if (myemail == "tjhickey724@gmail.com"){
       return Reviews.find({},{sort:{team:1,rating:-1}});
     } else {
       return myReviews;
