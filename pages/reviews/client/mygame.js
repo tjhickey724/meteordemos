6b42ba0d6435781764545a6d8ad4c3e45c84539d
tim <tjhickey@brandeis.edu>
2016-11-21 11:51:01 -0500

    changed order of reviews

--- pages/reviews/client/mygame.js
+++ pages/reviews/client/mygame.js
@@ -1,6 +1,6 @@
 function userEmail(){
   var user = Meteor.user();
-  if (user.services.google){
+  if (user.services && user.services.google){
     return user.services.google.email;
   } else if (user.emails) {
     return user.emails[0].address;
@@ -13,10 +13,12 @@ Template.mygame.helpers({
   myreviews: function(){
     var myemail = userEmail();
     var myProfile = Members.findOne({email:myemail});
+    console.log(myemail);
+    console.dir(myProfile);
     var myReviews = Reviews.find({team:myProfile.team});
     console.dir(myReviews.fetch());
     if (myemail == "tjhickey@brandeis.edu"){
-      return Reviews.find({},{sort:{team:1}});
+      return Reviews.find({},{sort:{team:1,rating:-1}});
     } else {
       return myReviews;
     }
