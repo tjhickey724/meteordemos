6559c5d141775d7abe24d74c70c7177f77ee20ae
tim <tjhickey@brandeis.edu>
2016-12-01 14:30:16 -0500

    added images to the game links

--- server/startup.js
+++ server/startup.js
@@ -4,88 +4,102 @@ teams =
 		title:'The Flight of Boxy',
 		teamname:'The Terabytes (formerly Blue Steel)',
 		reviewers:[],
-		url:"https://sites.google.com/a/brandeis.edu/blue-steel/home/rc1"
+    url:"https://sites.google.com/a/brandeis.edu/blue-steel/home/rc1",
+    img:"https://5021efc4-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/blue-steel/rc1/Desktop%2011.20.2016%20-%2023.36.08.03.png?attachauth=ANoY7coPcvHVmqaIVBaV4gxzdxu9AVgveuK8WKyW3Pd2oTo1ySUPm0Ude23P1gRDLBYsIXlWJzQ2VYUDmcIgG87htErusE0vqG-mDdtEDP62B3e-gXzW5dUza6upJWI9HlYx39kTGg4WV596Q-y2yR_L6MF4yUAUUI-eLbFHb-JeilM_2U1lDUKc049QwtzO7OPR3s59_tXFBdluQYl5U06Yc3DJlYx2kMRWInSV0SpDqG9EXPm7N_4d2mIc_iPPWusGtJn_5FEf&attredirects=0"
 	},
 
   { num:2,
 		title:'Snowball Fight',
 		teamname:'75% Korean',
 		reviewers:[],
-		url:"https://drive.google.com/drive/folders/0BxhLJwT_SEqTTEVPdFBMd0wtRmc?usp=sharing"
+    url:"https://drive.google.com/drive/folders/0BxhLJwT_SEqTTEVPdFBMd0wtRmc?usp=sharing",
+    img:"/images/snowballfight.png",
 	},
 
   { num:3,
 		title:'Room Escape',
 		teamname:'PrimeXL',
 		reviewers:[],
-		url:"https://sites.google.com/a/post.massbay.edu/clonedigitalimagen/"
+    url:"https://sites.google.com/a/post.massbay.edu/clonedigitalimagen/",
+    img:"https://80deeba4-a-54de6a2e-s-sites.googlegroups.com/a/post.massbay.edu/clonedigitalimagen/business-website-products/Screen%20Shot.png?attachauth=ANoY7cryPinngUxhCGD-RFLAFCGT97yr3IBdPklbZiR6R7ZwzDyHwELVxU4t7tSzNvy2tj1adZx9uDVU7zO_t9zqivEM1336UWxdjs33eEbXBYuDqWtT1EDfDnKyT6sCnD_aueXdvSrKqEQe84uHbZKoN8OwtVEywZYswPT2myIn20X0CJRMClRB8Zmnbg-cCpzW8LlZ-p-F0NvgjzP_wOkBfkeQUeN6A9D9-LPmVYV3NWF4I2KNrYxxlHHg326aH47qyURt8npn&attredirects=0"
 	},
 
   { num:4,
     title:'Totoro',
     teamname:'Totoro',
     reviewers:[],
-    url:"https://sites.google.com/a/brandeis.edu/totoro/finalgame"},
+    url:"https://sites.google.com/a/brandeis.edu/totoro/finalgame",
+    img:"https://49ced7a5-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/totoro/finalgame/Screen%20Shot%202016-11-20%20at%203.21.28%20PM.png?attachauth=ANoY7coqwAEhzPettPRV_YvRlNRSh78mGAi7j1kzw9rHyCE0ZBosvwZ2aAMI8ICXvrvdM3vYF80Hy9MFphipnYWhh1pZgX8yhYlqa4HmJw3G8kMQspXSVL5LTgqfXDFy2Qd3LhbVLkyj5IC08JubjWNcvOIYk-ScWD8ex89JfYCQ0iKEJZrG3DIMiUwcS7cwsyTfVqWIf8S18p8zOluFiUkkaZhYpCv_Tv_UXgCe39QgIIB3fFIhniy99OQZoMk6hWbAy1-mJZKB&attredirects=0",
+  },
 
   { num:5,
     title:'Pokemon World',
     teamname:'LandWorks',
     reviewers:[],
     url:"https://sites.google.com/a/brandeis.edu/landworks/home/hw9rc1",
+    img:"https://0dfb2923-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/landworks/home/hw9rc1/lvl2.png?attachauth=ANoY7cpE_m3oVZZFcst49E-pPnulsF32bAKOUOcocmfM-QPikZcYMnXabNWtlCX6tv6w0gTRh_uZFT7BI805O3qAS1IbI6UiHkiSf7bre6w2zoIFfQbMV9BRW5KANApx_X4ulIL2lYmLvtp4MQZQrfRv8bSwx26uxk0W3nKPP4Y0Abuh8D3ev_4mHqo10PMgUXanEcHTo4Npbtw78fp1J6KTTnwx_N24-A%3D%3D&attredirects=0"
 	},
 
   { num:6,
 		title:'Invaders of Space',
 		teamname:'Picks_R',
 		reviewers:[],
-		url:"https://sites.google.com/a/brandeis.edu/picks_r/rc1"
+    url:"https://sites.google.com/a/brandeis.edu/picks_r/rc1",
+    img:"https://3b0cd083-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/picks_r/rc1/Screenshot%20%288%29.png?attachauth=ANoY7cq1L9rtfOUEY42uud7dj_o2MP3qOliWhRlYs0BAqCvUlUOa0Bg_gunC68Ltvn2oP4YdOEjTTRChHcLKhshA9-44cdVQ5V43iMkesg6aagEGJS0jmQLCsoG2sNrpcyxsd_sArOa73Yhvw0Ue1xLevAVLdACJHyGb0wIkzBe3YLnozNN-xgqQncD5AjBR_fc36R_4cQiDcGEmydEzJc510m1cJZZjOG-N--fFT7Tpz2Agiu6m3Kc%3D&attredirects=0"
 	},
 
   { num:7,
     title:'Find the Landscape',
     teamname:'LW Squared',
     reviewers:[],
-    url:"https://sites.google.com/a/brandeis.edu/lw-squared/rc1-page"
+    url:"https://sites.google.com/a/brandeis.edu/lw-squared/rc1-page",
+    img:"https://6ffbed0e-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/lw-squared/rc1-page/Screen%20Shot%202016-11-19%20at%206.52.58%20PM.png?attachauth=ANoY7cp-sMsqVbvqc6prPlSLjwKtY8qQV7qBYHUoDhbmR7Fw8j7Qnp-yU9hqr7i8pDWPpuhJfmQIhfGMJeEjbqBxIA5AMw11NhXWqU3VXHgi4S8C9jFbecRob0GuoBElUQ6tk8vSgNOLFDwGKhlEJ9AZiLv2mK1VC40LkS5QPxnMYns8XIi_yXjmC752c1Qb3NVzNtGTdZNFQE7Jq2PL-rNpwD_SUiiu86K2a-MPD2FxqI4VJmDk83s11qbsNMDdqG4UcklebPZp&attredirects=0"
   },
 
   { num:8,
 		title:'Harry Potter and His Escape',
 		teamname:'Project Mayhem',
 		reviewers:[],
-		url:"https://sites.google.com/a/brandeis.edu/cosi164team8/home"
+    url:"https://sites.google.com/a/brandeis.edu/cosi164team8/home",
+    img:"https://b15de626-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/cosi164team8/rc1/Screen%20Shot%202016-11-20%20at%2010.35.29%20PM.png?attachauth=ANoY7cq_cXcfUvJBPm82U4NU0QyFEaIaYZJLfhZHrd7P3_I9U3Gaa1MKIHAV_O5jP3GMW2xx0mQXdl6-cnKvGx-uArSgo7PD8uDXWcqWNR1jcrPcTMzk_dLYw-e6lXvHDS540aIaA3ETqluBX_imUW1k9pLNydpCJKuj-98Tk-0ws3LkTXDclqu-vwTvoORjyzEX5ObT_zkZeSPkO9PlQ-KKQsZXI_WLRYtCysud7N-WVzoyOLU7NPunehJewnqIm_eE9ixmX_vV&attredirects=0"
 	},
 
   { num:9,
 		title:'Brandeis Outbreak',
 		teamname:'Chief Nish',
 		reviewers:[],
-		url:"https://sites.google.com/a/brandeis.edu/chief-nish/hw7/release-candidate-1"
+    url:"https://sites.google.com/a/brandeis.edu/chief-nish/hw7/release-candidate-1",
+    img:"https://5a887f69-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/chief-nish/RC1/15153132_10207554337325551_222530826_o.png.jpeg?attachauth=ANoY7covMjSGrGwG70wVsWAZlN2KfY69ziGyRWDyBh7QRHkQlg-klXcaBhqWW06-mpFaRzd-SMo1sieZd8sgKlVQG49YALmESZEt5uW9vqaCO79uPOC_cI6QhW9Mkk1ztBY-CZP74RD3o25LfmJVJnv0_qd_5_Oyl5XhY_RXluDUo17ro_lVVTlKJwj5KKLm4HdSodFwEGUG-TpEdLCkFtlZXNQbB_ThYn8_6AKqM91VeOKNc2SGq3rOXk8wkV7ujhtUS8SOaG3r&attredirects=0"
 	},
 
   { num:10,
     title:'Cat Assassin',
     teamname:'Team Ten',
     reviewers:[],
+    img:"https://92d753b8-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/cat-assassin/home/rc1/3.JPG?attachauth=ANoY7crVu9zsmjTY0jLWCVmKvdWP5OakCPsEbc0VaFz_fr4Tx9Qvuik37091moR0kL_MQxHid8qBp2nDLyg2SJtHFi_jIn3YeiazhUGJwE4LyK5HRbtDSBCDLH8uRncpGjt7TYmpp0BHyGpVokAWvjH_FSmYh7rwCJ-p_3QedXbh0J2tDj404sScrjGYYRDiyEXDHSstxVZqSgxnC3bMOmrnuZQTVaIi1g%3D%3D&attredirects=0",
     url:"https://sites.google.com/a/brandeis.edu/cat-assassin/home/rc1"},
 
   { num:11,
     title:'Quantum Escapement',
     teamname:'MA4^2PhD',
     reviewers:[],
-    url:"https://sites.google.com/a/brandeis.edu/ma43phd/home/rc1"
+    url:"https://sites.google.com/a/brandeis.edu/ma43phd/home/rc1",
+    img:"https://7869b1cc-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/ma43phd/home/rc1/qe-01.jpg?attachauth=ANoY7cr0xiYiXGjxwRwNjvt-imqaWS_5EMyZ_5Inr-hTELAL-k9HZZ9erYXygGcmKgE8x2tHXYdkTD3tZKvCT0eXluliv4I99on0Qkfht9_qBuhMyoYumfK3qk2-SjuZEzuobY-VrW7fnomApuA5YyhxEOv-C3TrGaGdAJojzxbx8EcHPMMd8-PTetogqVa04S3NVp78bCZ0zeNoqS-fG3PjKFpVPIIMtg%3D%3D&attredirects=0"
   },
 
   { num:12,
 		title:'Wander',
 		teamname:'Fireball',
 		reviewers:[],
-		url:"https://sites.google.com/a/brandeis.edu/fireball/homes"
+    url:"https://sites.google.com/a/brandeis.edu/fireball/homes",
+    img:"https://d2aa8afe-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/fireball/rc1/Screen%20Shot%202016-11-20%20at%206.19.40%20PM.png?attachauth=ANoY7crpNbFzfnFmtpGSYmpGUVUuInkyq3UCSDWJMmqTW0kcGTl4Mb1BFAPcCnmWB2rCJvhwMTpz8M4tidAvDyPhiEzo_jzhLjcWB9T6BIlij9gSrclhlAHYp8MNEFqxOSlybRm-Z1zqSRN5hANXiv7aP2VH_MAz0m6AzZpYtscFaTs_LX9y2HRbzNP1-o3ggdkiAi3p_Y0h1wo3O0ngBvWYNxWvrJEk3e1PwAJz5gY8m1-8hkMZ7pDc-5jnrB7TiayGon0VOi6y&attredirects=0"
 	},
 
   { num:13,
     title:'Superman Travelling the Sky',
     teamname:'Forensic Heroes',
     reviewers:[],
+    img:"https://sites.google.com/a/brandeis.edu/forensic-heroes/_/rsrc/1479688721740/cr1/level.png?height=222&width=400",
     url:"https://sites.google.com/a/brandeis.edu/forensic-heroes/cr1"},
 
   { num:14,
@@ -93,26 +107,30 @@ teams =
     teamname:'Team 14',
     reviewers:[],
     url:"https://sites.google.com/a/brandeis.edu/yumeworks/home/rc1?pli=1",
+    img:"https://205eba67-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/yumeworks/home/rc1/Screen%20shot%20of%20the%20game.png?attachauth=ANoY7cr9s9YnfL1_DWausOFoHyYXx1j5J0PzHK03jLPENN_D3AHqDrASlHZXYM0e6fJC6GjyBh9c25PjNV-8Rv6_Zg_fx2xQuVlph-JxTR_pzQ8oGBzgiNzo4XuZuhHVUwE0ocNZVF-P8Z5qMrPMTc8yASZ9pX999mC43yZDeuZdzZ5s_z7qq_RJfBB5CtQ44zN6Xtge_1G_YqUdx6hPaducp5OwQtnpq0qzMQJEdB7urDvBDZ3RBHY%3D&attredirects=0",
   },
 
   { num:15,
 		title:'Final Crushing',
 		teamname:'Chimpanzee-Brandeis Co',
 		reviewers:[],
-		url:"https://sites.google.com/a/brandeis.edu/chimpanzee-brandeis-co/home/rc1"
+    url:"https://sites.google.com/a/brandeis.edu/chimpanzee-brandeis-co/home/rc1",
+    img:"https://51ddf590-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/chimpanzee-brandeis-co/home/rc1/Screen%20Shot%202016-11-20%20at%208.01.31%20PM.png?attachauth=ANoY7crfz9ezxr_IEXeJTORAgL3lRyRqohzuQY_nCHlkHktkGNFUmMn90LSWxYRfcof_k8AsFsNgpkpNLj4bk4eVaTkKNn5yFiGK3SPvV4cbIv-Q7S_vM5jkM324PKCJLFSPa6GJlNfmfCrpcNglbv1tunFhKsP8BUdFVA17bNRK_ENI_AvUdsJK9Ohut3AMTtGwPaIAzTLLs1Vfdw7v0ZKRYS5LLVZSvGhGrIwuXaTyJ3xVlwZiiGCnNly00mcuBa-EvXBek_AO1zgIZVEBpDCm5H6uTXG0mw%3D%3D&attredirects=0"
 	},
 
   { num:16,
 		title:'Project Time Raft',
 		teamname:'Lonely Pirate Productions',
-		reviewers:[],
+    reviewers:[],
+    img:"https://a50e05f1-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/lonelypirateproductions/current-projects/release-candidate-1/2016-11-20%20%2810%29.png?attachauth=ANoY7cqeTLUHmqDkAaYqGjjYtA4pQXVvFqwZH4mh5LvM1QTYMtBi6qK3ixFwjasEENRD-0qpC5W4fYeNGjZJbJnewZLrExE2UMNsTghuLMAmuhxIAyyn2YeO10If-pQAYKyLh_59FmZoABIKb_5t-fu9RqhcbSymgaJ0O9ryHNC_QFaCFGUbT-bsd_MCAe6hDZ_VUDycC9uB7mKWN8r0PTbLO3bomPBGOOzU4nmNXivTnRDWz4Bh71T56a2L8gYhZcfuwl8n1yi3NDdof8ZHhgZUVhgZBC_V7xGgRtSIImpUBqnM0MyvH_E%3D&attredirects=0",
 		url:"https://sites.google.com/a/brandeis.edu/lonelypirateproductions/current-projects/release-candidate-1"},
 
   { num:17,
 		title:'Spacecraft Shooter',
 		teamname:'Wavedash',
 		reviewers:[],
-		url:"https://sites.google.com/a/brandeis.edu/wavedash/home/rc1",
+    url:"https://sites.google.com/a/brandeis.edu/wavedash/home/rc1",
+    img:"https://sites.google.com/a/brandeis.edu/wavedash/_/rsrc/1479666566324/home/rc1/imageedit_2_6939863413.png?height=239&width=400",
 	},
 
   { num:18,
@@ -120,6 +138,7 @@ teams =
     teamname:'Team 18',
     reviewers:[],
     url:"https://sites.google.com/a/brandeis.edu/two-point-0/hw9---release-candidate-1",
+    img:"https://ab2d04d3-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/two-point-0/hw9---release-candidate-1/Screen%20Shot%202016-11-19%20at%207.25.49%20PM.png?attachauth=ANoY7crBMCRbZ-Vt7KbpKH9ZsskvgRgGG_km87z4ZuJPesli-ZrRggu1iRsH0FxvBC6KFwKv_LvKRAjo3BRsnRDuXxtEHNYTNj0fdC3ouWxkiKRLmuFYn1K8E93oseqq-Q8aGsXxzcOBJGNQFNjS4_z0MQaH34p90GsGutgOjRztYBKb_l301GpTxf4Y9aB4Hx0qrnXs1zIqyw_I146XPre3lrNDH9kYrVTfXUaQ1EHSWHSgLb0PoivUQo0bXgP0HOIkTmGawJg9TnXadxH8JJx4SUA_q-ltMw%3D%3D&attredirects=0",
   },
 
   { num:19,
@@ -127,6 +146,7 @@ teams =
     teamname:'FungiStudio',
     reviewers:[],
     url:"https://sites.google.com/a/brandeis.edu/fungistudio/rc1",
+    img:"https://047d8324-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/fungistudio/rc1/Screen%20Shot%202016-11-20%20at%208.05.42%20PM.png?attachauth=ANoY7coNj3HW-9aN8dhdwuoGIs9RS1cnC9FpvWKr2ESjP0i44xp4lzhUw-ouMBC3ycvM7lGqcGdiTLR7ZIVK-GiUBFY6j2Ozeolak3ZzE3arWaXxP2mHJ_Z11BhWdGoA1X09-mIiBW0FXe3pSJvznAawdYyLQdK2nQuo1EwU2dL5JUxLqTrjdWnSmSo4IVJkzd1COArZ6WPncONxuqXiUQV3SuMEDcG7iKiAEBW9EeiQ97pha8leLTMQfrIe84F-ycy_bw2ASjic&attredirects=0",
   },
 
   { num:20,
@@ -134,34 +154,39 @@ teams =
     teamname:'Team 20',
     reviewers:[],
     url:"https://sites.google.com/a/brandeis.edu/nightmare/home/hw9",
+    img:"https://f50da56e-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/nightmare/home/hw9/Screen%20Shot%202016-11-20%20at%2015.23.15.png?attachauth=ANoY7crZplwgI47LEVPdctWyxZboCL1KdQzXKscznSoTY4nv_8_wxSCcggn_lgFRIgoqThh7L4Leg3PKId5h7_43NqK_R8XUFzPsWxRV0khedDQj7i14l7w6pvQPT0E0g6MDNjjYM1jw2KiEnJh4E4N5m-o-Or6Oc3qN8YaP83CqyuJUn2KcW6u_DOIKWDBIss0jzD64ZhaD2MrDvJyl8jz_QEgJP6nGW_GP4IR_b6z5MEBhehakxHnXzp-XCvJ_bHscn8oUnsEN&attredirects=0",
   },
 
   { num:21,
 		title:'Suzanne: The Great Escape',
 		teamname:'Damaged Engineers',
 		reviewers:[],
-		url:"https://drive.google.com/drive/folders/0BzDAbhgHMShKZUdOMXdDQjVKOHM?usp=sharing"
+    url:"https://drive.google.com/drive/folders/0BzDAbhgHMShKZUdOMXdDQjVKOHM?usp=sharing",
+    img:"/images/suzanneGreatEscape.png",
 	},
 
   { num:22,
 		title:'Gonzales: El Bigote del Sur',
 		teamname:'Los Juegos Buenos',
 		reviewers:[],
-		url:"https://sites.google.com/a/brandeis.edu/los-juegos-buenos/home/rc1"
+    url:"https://sites.google.com/a/brandeis.edu/los-juegos-buenos/home/rc1",
+    img:"https://bedee69a-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/los-juegos-buenos/home/rc1/mustache.png?attachauth=ANoY7cquOX2bQtP0dMkMhjYNIu_6TJOY3bN1rosgzjAJflH621-EQEUHlekK2Z0up0K0ZEWdQx9rBGDc7tIeT2Jka_I6cQ4TrCyAog5TjSXy-eAdPbosA6ns1qMPGZLakon3qrdx8TRR8oTONg8C8ZcOvgcJd1jD5vjgwoRKfXZmNeN0E6OhSc06wDLQna4qtVk0v7Lcq1_F85viaMwd6hO8qDLRINsaz9TrbDhCqt_EhdhUw88MqCE%3D&attredirects=0"
 	},
 
   { num:23,
     title:'testing',
     teamname:'TimsTests',
     reviewers:[],
-    url:""
+    url:"",
+    img:""
   },
 
   { num:24,
 		title:'Spike Evasion',
 		teamname:'Underwater Squad',
 		reviewers:[],
-		url:"https://sites.google.com/a/brandeis.edu/underwater-squad/home/release-candidate-1"
+    url:"https://sites.google.com/a/brandeis.edu/underwater-squad/home/release-candidate-1",
+    img:"https://27402153-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/underwater-squad/home/release-candidate-1/spikeevasion_screenshot.png?attachauth=ANoY7crRP5GLg_Y7R3w-y8ci_CP_oIFaQ8HMAC6D_yMQah-6EkA3DU8bXpIxXfQrsx7M3Zqlialw2tDV-4jZ4KhXQHpr-iQKfknXwCW2EPz--KYEnsET5nqNm6I3bRMWdbozluL3aHetUvzifEaPFSw0XCkGrwP1f6s8vJj3hqnU2ch2u0d2cO1A4X3EzR2253RTuAwV7MeMrZ6gIED3B4hMQ2gLn-fQ7-F6bPCipABH9nyRE33IiC53TZnYzvd8mZvttqvp0iznKJVysvSCJ0rBVmrZlxuriQ%3D%3D&attredirects=0"
 	},
 
   { num:25,
@@ -169,12 +194,14 @@ teams =
     teamname:'Turbo Tech',
     reviewers:[],
     url:"https://sites.google.com/a/brandeis.edu/turbotech1/home/hw9-rc1",
+    img:"https://ab4fd54a-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/turbotech1/home/hw9-rc1/Screenshot.png?attachauth=ANoY7cpqnxkpSFgOdq-iKGmCmJaO1ROfYujBdeQE3c2R7cVyRblGRuENNTWQomnv8dM1WAdMecZxWPEJHawMeVIWI53Gc7EyiHchjMX2RL55FgAg5PUU1PUY8CBxlHyw0Ln0Dn3LRDVmM7RmYJ-OisksvxGxbuyrinJZ2hb93006gFTK5gHJ1G7L7HtKQsdZjXwUiuPgzOwPjLeDMpEf9HCgLwCc-yKEtH6-phR37eOHXJaUMhYHTF4%3D&attredirects=0",
   },
 
   { num:26,
 		title:'Animatrix',
 		teamname:'Animatrix Productions',
-		reviewers:[],
+    reviewers:[],
+    img:"https://55348b60-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/animatrix/home/assignment-9/Game%20Snapshot.PNG?attachauth=ANoY7craWQ-RteUaMITpCNKQzTWaKDo8E2NFFjpOiHVI6SZvDnlvmVupMORGPkh9hpdEC6ZUpHv9lRx9q33fBo3LK6CB6Uf5t-yuhupW9YJjKy7aj1tjqQOxedFgpzA5j5shRs1RIM_ST5ZLjX6wFaLU4W2BJTymNtqMWlfAQcbg9obV6yVhiuW4A23iNpaf4jYvVV5DOSV9sH0d2Qe-jSx71qcd7QjxxE0Th9pNE5W1I-vHJg-jtXc%3D&attredirects=0",
 		url:"https://sites.google.com/a/brandeis.edu/animatrix/home"},
 
   { num:27,
@@ -182,46 +209,52 @@ teams =
     teamname:'Golden Bear',
     reviewers:[],
     url:"https://sites.google.com/a/brandeis.edu/goldenbear/home/rc1",
+    img:"https://906ed9c3-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/goldenbear/home/rc1/Screen%20Shot%202016-11-20%20at%205.29.48%20PM.png?attachauth=ANoY7crqPWdtx3lJWaWQqh6EWn4NLzQO98NEwHQ-3k2CmdyBOlUt6qte9SNilxIAgB6F3koOw3ps0LZspGxolBQ81NyEjvgxMmRNvpoGsouesqLnLs0-eOrL_2pmb8We5XmSKQhSSGXM7fVeOV-vho2vnPlMaafo86VA63fXui6hRG_w3B12KBhFLlN9lHWwWAf-OP6f4_ROytxlhdcHwOCgW3-scEdOisVCICzQKEW-E0aNLHwAOoovxoUrn8GjNpy2rGezuvrf&attredirects=0",
   },
 
   { num:28,
 		title:'Super Mario Blender',
 		teamname:'Ultra Studios',
 		reviewers:[],
-		url:"https://sites.google.com/a/brandeis.edu/ultra-studios/business-website-services"
+    url:"https://sites.google.com/a/brandeis.edu/ultra-studios/business-website-services",
+    img:"https://ac07fe35-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/ultra-studios/business-website-services/mario.png?attachauth=ANoY7cpmypzJdkE3VOr178XauYLNbQ9r4YxFgWp3L0zudR635a2gtptYMSO-0e-FNVioZsviwocbxLBQZxYppaUPCVBSJBY354JrG9fEn5vGmWb0_tlxYIL_NYdh72e0voywVXeDrq3-e5MLupvNw-do7aB9LAQKiL403Q7WT-DVqES1FysMIGpzOG_BwY97lqwxHYYxZCrl6Wb3y67mpcmcDiyy41DV-vmoPYAG0HzRTdLjOkDaUuk0R84p9_Vs6UKYXoIPMkaD&attredirects=0"
 	},
 
   { num:29,
     title:'SuperApple',
     teamname:'SuperNova',
     reviewers:[],
+    img:"https://c67250ec-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/supernova/rc1/Screen%20Shot%202016-11-19%20at%2010.51.41%20PM.png?attachauth=ANoY7cocz1Hb8unysiHRr7rN_FT5C9kgxGpuMEUzHlJE8aGsMeB_MGiF9g21IIOk5x7Zys8Vs5U1KB1cjJJI1pqZGXmcxSZCLdCMiy7Nv8uyegCF1HZIjhs9AJG_6d344rKWc90uE9Adzb7sS7DC0o3qro6xW97q599YBSbPCuQB5BvzxVlfJuARLlxhvYaWzX78xTUQXn8-FQH4qC7hXfn10FfBUwxLpKpdSfsHbyP0saZUPxNM9WADlAyRo9aunTiYQdnaM_SX&attredirects=0",
     url:"https://sites.google.com/a/brandeis.edu/supernova/rc1"},
 
   { num:30,
 		title:'Go Bananas',
 		teamname:'Money Team',
 		reviewers:[],
-		url:"https://sites.google.com/a/brandeis.edu/gobananas/rc1"
+    url:"https://sites.google.com/a/brandeis.edu/gobananas/rc1",
+    img:"https://sites.google.com/a/brandeis.edu/gobananas/rc1/Screen%20Shot%202016-11-20%20at%206.45.49%20PM.png?attredirects=0"
 	},
 
   { num:31,
 		title:'Not Ur Ave. Game',
 		teamname:'AFK Productions',
 		reviewers:[],
-		url:"https://sites.google.com/a/brandeis.edu/away-from-keyboard/"
+    url:"https://sites.google.com/a/brandeis.edu/away-from-keyboard/",
+    img:"https://f5174e05-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/away-from-keyboard/products-services/15127411_10202664350366006_876295903_o.png?attachauth=ANoY7cr7cq_mKxUrEIPb95TdPurKKeIzsMaTHRmL9wxZGlDHrMpcgn_JotPAkOPvm9BZ3rN_F--XVeU3XXWSifZT2E0sjLKnszGgTeipGf40-Eo2GEaZjelLQeQvJ1zl3Rz9guX4sQSzS2lMAYR6uG759nmWGn2zYv-CfiOVvk_yaoTx61GXbWwkzzYf4977x5FvVr7E5XRUZPXIdT7XE9rs3jI9JI80dlcANkmhxACG_LgeNcet3dORAQWgM-URBMg_1bSZSShx8sbUmdiEYezPIKC0m2xUXg%3D%3D&attredirects=0"
 	},
 
   { num:32,
 		title:'Zombie Rush',
 		teamname:'Zombie Bros',
 		reviewers:[],
+    img:"https://7b7ddd4d-a-850057e4-s-sites.googlegroups.com/a/brandeis.edu/zombie-bros/rc1/RC1%20Screen%20Shot.png?attachauth=ANoY7crEqeyyCykLl_UH8hVDb2ki3rfoOK-N0ynCsUzLfzRQZnJqbyKD5ul3AVtAqOFZPG8oCisGJ8ZLow-vx7SC0b4NOpJ1sa89PEu0nTptiXGEi15a3iMqxXlM6odaBu8A2ySgIKut2smzuMb47mrj_Azgz5mXlxkEfcHbIYbq7uS9EXkjYLVdHAWu0NO8NRicJZv8on1TMFpj3jKKi_9Q0tIDrksk41ocUHuGfCFpW8vhjCMRSsQ%3D&attredirects=0",
 		url:"https://sites.google.com/a/brandeis.edu/zombie-bros/"
 	}
 
 ];
 
 var members =
-[
+    [
 {team:9, name:"Acharya, Nishan",	email:"acharyan@brandeis.edu"},
 {team:17, name:"Alvarado, Joshua",	email:"jalva@brandeis.edu"},
 {team:12, name:"Amrien, Conor",	        email:"rconora@brandeis.edu"},
@@ -356,7 +389,7 @@ Meteor.methods({
 	"updateTeams":function(){
 		teams.forEach(function(s){
 			console.log("updating "+s.title);
-			Teams.update({num:s.num},{$set:{title:s.title,teamname:s.teamname,url:s.url}});
+			Teams.update({num:s.num},{$set:{title:s.title,teamname:s.teamname,url:s.url,img:s.img}});
 		});
 		members.forEach(function(s){
       console.log(s.name);
