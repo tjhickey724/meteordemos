5ed366448378e8b61485e97b18e25b570d080b60
tim <tjhickey@brandeis.edu>
2017-07-04 15:49:41 -0400

    completed initial version of mini-Facebook with profile, friends, and posts

--- pages/facebook/client/fbtimeline.js
+++ pages/facebook/client/fbtimeline.js
@@ -3,7 +3,7 @@ Template.fbtimeline.helpers({
     var myFriends = Friends.find({owner:Meteor.userId()}).fetch();
     var myFriendsList = _.map(myFriends,function(x){return x.friend});
     console.dir(myFriendsList);
-    return Posts.find({owner:{$in:myFriendsList}})
+    return Posts.find({owner:{$in:myFriendsList}},{sort:{createdAt:-1}})
     //return Posts.find();
   }
 })
--- pages/facebook/client/fbusers.js
+++ pages/facebook/client/fbusers.js
@@ -3,15 +3,16 @@ Template.fbusers.helpers({
   users(){return Profiles.find();},
 })
 Template.fbperson.helpers({
-  isFriend(){return Friends.findOne({owner:this.u.owner,friend:Meteor.userId()})}
+  isFriend(){return Friends.findOne({friend:this.u.owner,owner:Meteor.userId()})}
 })
 
 Template.fbperson.events({
   "click #js-friend"(event, instance){
-    Friends.insert({owner:this.u.owner,friend:Meteor.userId()})
+    Friends.insert({friend:this.u.owner,owner:Meteor.userId()})
   },
   "click #js-defriend"(event,instance){
     var z = Friends.findOne({owner:this.u.owner,friend:Meteor.userId()});
+    console.log('removing'); console.dir(z);
     Friends.remove(z._id);
   },
 
