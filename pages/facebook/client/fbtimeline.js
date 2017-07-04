f7b4e94e087a8b26f41e94b6a3da385f36c9236f
tim <tjhickey@brandeis.edu>
2017-07-04 11:53:39 -0400

    added friends and posts

---
+++ pages/facebook/client/fbtimeline.js
@@ -0,0 +1,25 @@
+Template.fbtimeline.helpers({
+  posts(){
+    var myFriends = Friends.find({owner:Meteor.userId()}).fetch();
+    var myFriendsList = _.map(myFriends,function(x){return x.friend});
+    console.dir(myFriendsList);
+    return Posts.find({owner:{$in:myFriendsList}})
+    //return Posts.find();
+  }
+})
+
+Template.fbmakepost.events({
+  "click #submit"(event, instance){
+    var profile = Profiles.findOne({owner:Meteor.userId()});
+    var now = new Date();
+    var text = instance.$("#fb-posttext").val();
+    var post = {
+      owner:Meteor.userId(),
+      name:profile.name,
+      createdAt: now,
+      text: text
+    };
+    console.dir(post);
+    Posts.insert(post);
+  }
+})
