b4bdaf936aba3e7a7aef9145ae1fbc478b1fe26f
tim <tjhickey@brandeis.edu>
2017-07-08 22:43:56 -0400

    improved menus

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -1,7 +1,6 @@
 const team =
   [
     {name:"Tim Hickey"    , email: "tjhickey@brandeis.edu" },
-    {name:"Danielle Pike" , email: "dapike13@gmail.com" }
   ];
 
 Template.about.helpers(
--- pages/facebook/client/fbprofile.js
+++ pages/facebook/client/fbprofile.js
@@ -1,16 +1,25 @@
+
+
 Template.fbprofile.helpers({
   user(){
     var theProfile = Profiles.findOne({owner:Meteor.userId()});
     if (!theProfile){
       var k = Profiles.find().count();
-      Profiles.insert({name:"anonymous"+k,owner:Meteor.userId()});
-    } else {
-    return theProfile
+      Profiles.insert({name:"anonymous"+k,program:'other',owner:Meteor.userId()});
+      theProfile = Profiles.findOne({owner:Meteor.userId()});
     }
+    return theProfile;
+
    },
 
 
+
+
 })
+
+
+
+
 
 
 Template.fbinfo.events({
@@ -18,11 +27,14 @@ Template.fbinfo.events({
     name = instance.$('#js-name').val();
     url = instance.$('#js-url').val();
     bio = instance.$('#js-bio').val();
+    program = instance.$('#js-program').val();
     console.log('just read '+name);
     this.user.name = name;
     this.user.url = url;
     this.user.bio = bio;
+    this.user.program = program;
     var zz = Profiles.update(this.user._id,this.user);
-    Router.go('/fbuser/'+this.user._id);
+    //Router.go('/fbuser/'+this.user._id);
+
   }
 })
